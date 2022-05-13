59a6e1e6aef1d4fdf3ab45a5fd2f7596d2fba8d3
Alb3rnaru <moreno.alberto907@gmail.com>
2022-05-13 22:42:18 +0000

    :sparkles: Subiendo el Random Card Generator incluyendo los 3 retos adicionales

--- src/app.js
+++ src/app.js
@@ -5,7 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let suits = ["diamond", "spade", "heart", "club"];
+
 window.onload = function() {
   //write your code here
+  let topDiv = document.querySelector("#topIcon");
+  let botDiv = document.querySelector("#botIcon");
+  let suitsPosition = Math.round(Math.random() * 3);
+  topDiv.className = suits[suitsPosition];
+  botDiv.className = suits[suitsPosition];
+  let numberDiv = document.querySelector("#card-number");
+  let number = Math.round(Math.random() * (12 - 1) + 1);
+  //console.log(number);
+  numberDiv.innerHTML = number;
+
+  setInterval(newCard, 10000);
+
+  function newCard() {
+    let suitsPosition = Math.round(Math.random() * 3);
+    topDiv.className = suits[suitsPosition];
+    botDiv.className = suits[suitsPosition];
+    let number = Math.round(Math.random() * (12 - 1) + 1);
+    numberDiv.innerHTML = number;
+  }
+
+  let button = document.querySelector("#newCardButton");
+  button.addEventListener("click", newCard);
+
+  let divCard = document.querySelector("#divCard");
+
+  let changingWidth = document.querySelector("#widthOfCard");
+
+  changingWidth.addEventListener("keydown", event => {
+    let newWidth = changingWidth.value;
+    newWidth = parseInt(newWidth);
+    if (event.keyCode === 13) {
+      if (newWidth < 120 || newWidth > 1000) {
+        alert("Please insert a value between 120 and 1000");
+        changingWidth.value = "";
+      } else {
+        divCard.style.width = newWidth + "px";
+      }
+    }
+  });
+
+  let changingHeight = document.querySelector("#heightOfCard");
+
+  changingHeight.addEventListener("keydown", event => {
+    let newHeight = changingHeight.value;
+    newHeight = parseInt(newHeight);
+    if (event.keyCode === 13) {
+      if (newHeight < 120 || newHeight > 1000) {
+        alert("Please insert a value between 120 and 1000");
+        changingHeight.value = "";
+      } else {
+        divCard.style.height = newHeight + "px";
+      }
+    }
+  });
+
   console.log("Hello Rigo from the console!");
 };
